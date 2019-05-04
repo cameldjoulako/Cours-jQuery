0341bf4a267c95b512f5d90f492d0e394926ff40
cameldjoulako <cameldjoulako17@gmail.com>
2019-05-04 23:49:22 +0100

    edit and add much content

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 	"Ecrire moins, faire plus"
  */
-/*Quand le document(la page htm) est chargé(prête) alors la fonction suivante est executé:*/
+/*Quand le document(la page html ) est chargé(prête) alors la fonction suivante est executé:*/
 $(document).ready(function(){
 	console.log('jquery.js bien chargé');
 
@@ -160,11 +160,99 @@ $(document).ready(function(){
 	//modifier un attribut
 
 	/*Ajouter des nouveaux elements html a notre page*/
-	//ajout paragraphe
+
+	//ajout paragraphe dans le div ayant pour class .love
+
 
 	$(".addparagraph").click(function(){
 		$(".love").append("<p>My favorites languages: Java, PHP, JavaScript</p>");
+				  /*
+				  .prepend()
+				  .after()
+				  .before()
+
+				  */		
 	});
 
+
+	/*Retirer un element html et son contenu*/
+
+	$(".retirer").click(function(){
+		$(".removeElements").remove();
+				  
+	});
+
+	/*Vider le contenu d'un element*/
+	$(".retirer").click(function(){
+		$(".removeElements").empty();
+	});
+
+
+ 	/*JQuery et le CSS*/
+
+ 	/*Ajout  d'une classe */
+
+ 	$("#ajoutClass").click(function(){
+ 		$("#pa1, #pa2").addClass("green bold");
+ 	});
+
+
+	/*Retrait  d'une classe */
+
+ 	$("#retirerClass").click(function(){
+ 		$("#pa1, #pa2").removeClass("green bold");
+ 	});
+
+ 	//ajout et retrait
+ 	$("#ajoutRetrait").click(function(){
+ 		$("#pa3").toggleClass("green bold");
+ 	});
+
+
+	/*jQuery et l'arboressance*/
+	$("#manage").click(function(){
+		//alert("Bonjour");
+
+		//*modification de la couleur de bordeure du parent du paragraphe 1 au click*/
+		//$("#paragraphe1").parent().css("border-color", "green");
+
+		//*modification de la couleur de bordure de tous les ancêtresdu paragraphe 1 au click*/
+		//$("#paragraphe1").parents().css("border-color", "green");
+
+
+		/*selection des enfants d'un div*/
+		//$("#div1").children().css("border-color", "green");
+
+		/*selection des enfants specifique*/
+		//$("#main").find("p").css("border-color", "green");
+
+		/*Selection des elements qui partagent le meme parent*/
+		//$("#paragraphe1").siblings().css("border-color", "green");
+
+		/*Selection du frere suivant imediat*/
+		//$("#paragraphe1").next().css("border-color", "green");
+
+		/*Selection de tous les paragraphes*/
+		//$("p").css("border-color", "green");
+
+		/*Selection du premier paragraph parmi plusieurs*/
+		//$("p").first().css("border-color", "green");
+
+		/*Selection du dernier paragraph parmi plusieurs*/
+		//$("p").last().css("border-color", "green");
+
+		/*Selection d'un  paragraph en un idex precis*/
+		//$("p").last().css("border-color", "green");
+
+		/*filtrer la selection*/
+		//selection des paragraphe avec une classe precise
+		$("p").filter(".filter").css("border-color", "green");
+	});
+
+
+	/*Ajax et jQuery*/
+	$("#testAjax").click(function(){
+		$("#ajax").load("demo.txt");
+	});
 
 });
